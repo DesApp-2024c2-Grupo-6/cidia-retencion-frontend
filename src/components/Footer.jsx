68b1b82a54944e924be7ee441162ab7ef6bf2e16
Javier-I-Morales <javierignamorales@gmail.com>
2024-05-05 19:49:59 -0300

    FOOTER DE PRUEBA

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
 
-
-
+import { Box } from "@mui/material";
 
 function Footer() {
 
     return (
         <>
-
+            <Box
+                component="Footer"
+                sx={{
+                    position: 'fixed',
+                    bottom: 0,
+                    width: "100%",
+                    left: 0,
+                    height: '20vh',
+                    backgroundColor: '#333333'
+                }}>
+            </Box>
         </>
     );
 }
