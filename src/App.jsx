24f4be09020ad4e6db4a18d368a97befde972660
Javier-I-Morales <javierignamorales@gmail.com>
2024-05-06 16:41:46 -0300

    Cargo los cambios para volver a bajar main porque no me arranca la aplicación con el nuevo template

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
-//import React, { useEffect, useState } from "react";
-/*import { Box, Grid, Stack } from "@mui/material";*/
+
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer"
-//import { TopMenu } from "./components/TopMenu";
-//import { AppRouter } from "./AppRouter";
-//import { getCurrentWeather } from "./services/WeatherService";
-//import { WeatherIndicator } from "./components/WeatherIndicator";
+//import Header from "./components/Header"
+
 
 export function App() {
-  //const [weatherData, setWeatherData] = useState();
 
-  //useEffect(() => {
-  //  const fetchWeatherData = async () => {
-  //    const obtainedData = await getCurrentWeather('Buenos Aires');
-  //    setWeatherData(obtainedData);
-  //  }
-  //  fetchWeatherData();
-  //}, []);
 
-  return (
-<<<<<<< HEAD
-      <BrowserRouter>
-        <Footer />
-      {/*<Stack direction='column'>*/}
-      {/*  <Grid container direction='row'>*/}
-      {/*    <Grid item xs={12} md={8}>*/}
-      {/*      <TopMenu />*/}
-      {/*    </Grid>*/}
-      {/*    <Grid item xs={12} md={4}>*/}
-      {/*      <WeatherIndicator weatherData={weatherData} />*/}
-      {/*    </Grid>*/}
-      {/*  </Grid>*/}
-      {/*  <Box sx={{mx: { xs: 1, md: 4 }, my: 4}}>*/}
-      {/*    <AppRouter />*/}
-      {/*  </Box>*/}
-      {/*</Stack>*/}
-=======
-    <BrowserRouter>
-        <Header/>
->>>>>>> d015975439734833a2477da80e78de7906cfdc9b
-    </BrowserRouter>
+    return (
+
+        <BrowserRouter>
+          {/*<Header />*/}
+          <Footer />
+        </BrowserRouter>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,27 @@
-import logo from '../assets/img/Logo-UNAHUR.png';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
+////import logo from '../assets/img/Logo-UNAHUR.png';
+//import Button from '@mui/material/Button';
+//import ButtonGroup from '@mui/material/ButtonGroup';
 
-const buttons = [
-    <Button key="Configuraciones" variant="contained" onClick={changeOptionPage}>Configuraciones</Button>,
-    <Button key="two" variant="contained">opción 2</Button>,
-    <Button key="three" variant="contained">opción 3</Button>,
-    <Button key="four" variant="contained">opción 4</Button>,
-    <Button key="four" variant="contained">opción 4</Button>,
-    <Button key="four" variant="contained">opción 4</Button>,
-];
-// 
-function changeOptionPage(e) {
-    window.sessionStorage.setItem("mainContent", e.key);
+//const buttons = [
+//    <Button key="Configuraciones" variant="contained" onClick={changeOptionPage}>Configuraciones</Button>,
+//    <Button key="two" variant="contained">opción 2</Button>,
+//    <Button key="three" variant="contained">opción 3</Button>,
+//    <Button key="four" variant="contained">opción 4</Button>,
+//    <Button key="four" variant="contained">opción 4</Button>,
+//    <Button key="four" variant="contained">opción 4</Button>,
+//];
+//// 
+//function changeOptionPage(e) {
+//    window.sessionStorage.setItem("mainContent", e.key);
     
-}
-function Header() {
-    return (
-        <header>
-            <img src={logo} alt="UNAHUR" />
-            <ButtonGroup size="large" aria-label="Large button group">{buttons}</ButtonGroup>
-        </header>
-    );
-}
+//}
+//function Header() {
+//    return (
+//        <header>
+//            <img src={logo} alt="UNAHUR" />
+//            <ButtonGroup size="large" aria-label="Large button group">{buttons}</ButtonGroup>
+//        </header>
+//    );
+//}
 
-export default Header;
+//export default Header;
