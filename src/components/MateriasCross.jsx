88c846e314c43aa17f0916d413c516a6ba2cfd2f
cvitola <isrunaway@gmail.com>
2024-05-28 15:45:54 -0300

    Eliminado y agregado en listas

--- src/components/MateriasCross.jsx
+++ src/components/MateriasCross.jsx
@@ -1,61 +1,94 @@
-import { Box , TextField, FormLabel} from "@mui/material";
+import { Box , TextField, FormLabel, IconButton} from "@mui/material";
+import DeleteIcon  from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
 import '../styles/MateriasCross.css';
+import { useState } from "react";
 
 export default function MateriasCross ({isEdit, title, array}){
 
+    const [arrayB, setArrayB] = useState(array);
+    
+    const handleOnDeleteRow = (index) => {
+        const updateArray = array.filter((_,i) => i != index);
+        setArrayB(updateArray)
+    }
 
+    const handleOnAddRow = () => {
+        const newRow = {year: "", campo: ""};
+        setArrayB([...arrayB, newRow]);
+    }
     return(
-
         <Box className='card-materia'>
-                <h5 className="card-materia-title">{title}</h5>
+            <h5 className="card-materia-title">{title}</h5>
                 <div className="card-materia-item">
                     <FormLabel
-                    sx={
-                        {
-                            fontWeight: 'bold',
-                            maxWidth: 'calc(20% - 5px)', 
-                            fontSize: '12px'
-                        }
+                        sx={
+                            {
+                                fontWeight: 'bold',
+                                maxWidth: 'calc(20% - 5px)', 
+                                fontSize: '12px'
+                            }
                     }>Año</FormLabel>
                     <FormLabel
-                    sx={
-                        {
-                            fontWeight: 'bold',
-                            maxWidth: 'calc(20% - 5px)', 
-                            fontSize: '12px'
-                        }
-                    }>Campo</FormLabel>
-                </div>
-                
-                    {
-                        array?.map( registro => (
-                        <div className="card-materia-item">
-                            
-                            <TextField
-                            defaultValue={registro.year}
-                            variant='standard'
-                            sx={
-                                {
-                                    maxWidth: 'calc(20% - 5px)', 
-                                    '& .MuiInputBase-input': { 
-                                        fontSize: '12px', 
-                                        textAlign: 'center' }
-                                }
+                        sx={
+                            {
+                                fontWeight: 'bold',
+                                maxWidth: 'calc(20% - 5px)', 
+                                fontSize: '12px'
                             }
+                    }>Campo</FormLabel>
+                    {
+                        isEdit && (
+                            <IconButton
+                                color='success'
+                                sx={{ width: '25px' }}
+                                onClick={handleOnAddRow} >
+                                
+                            <AddIcon />
+                        </IconButton>
+                        )
+                    }
+                        
+                </div>
+                    {
+                        arrayB?.map( (registro, pos )=> (
+                        <div className="card-materia-item" key={pos} index={pos}>
+                            <TextField
+                                defaultValue={registro.year}
+                                variant='standard'
+                                sx={
+                                    {
+                                        maxWidth: 'calc(20% - 5px)', 
+                                        '& .MuiInputBase-input': { 
+                                            fontSize: '12px', 
+                                            textAlign: 'center' }
+                                    }
+                                }
                             inputProps={{readOnly: !isEdit}}/>
 
                             <TextField
-                            defaultValue={registro.campo}
-                            variant='standard'
-                            sx={
-                                {
-                                    maxWidth: 'calc(20% - 5px)', 
-                                    '& .MuiInputBase-input': { 
-                                        fontSize: '12px', 
-                                        textAlign: 'center' }
+                                defaultValue={registro.campo}
+                                variant='standard'
+                                sx={
+                                    {
+                                        maxWidth: 'calc(20% - 5px)', 
+                                        '& .MuiInputBase-input': { 
+                                            fontSize: '12px', 
+                                            textAlign: 'center' }
+                                    }
                                 }
-                            }
-                            inputProps={{readOnly: !isEdit}}/>
+                                inputProps={{readOnly: !isEdit}}/>
+                            {
+                                isEdit && (
+                                <IconButton
+                                    sx={{ width: '25px' }}
+                                    onClick={() => handleOnDeleteRow(pos)}> 
+                                    <DeleteIcon
+                                        fontSize="small"
+                                        sx={{ color: 'red'}} />
+                                </IconButton>
+                                ) 
+                            }                           
                         </div>
                         ))
                     }
