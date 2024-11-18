d0ec7ce861c33c0f644f45e802335db3abf69358
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2024-11-18 20:52:43 -0300

    cambio de vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    supported: {
-      'top-level-await': true
-    },
+  esbuildOptions: {
+    target: 'esnext'
   },
+  build: {
+    target:'esnext'
+  }
 });
