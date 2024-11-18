9ac2d022757673ff72b1d108b7d761d410e0a458
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2024-11-18 19:04:54 -0300

    cambiado config para ignorar error

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-},
-optimizeDeps= {
-  esbuildOptions: {
-    target: 'esnext'
-  }
-},
-build= {
-  target: 'esnext'
-}
-)
-
+  esbuild: {
+    supported: {
+      'top-level.await': true
+    },
+  },
+});
