d962df292127cf0e82c4e72fc748b266c3b3d05d
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2025-04-12 22:35:49 -0300

    agregados endpoints para alumnos del mock

---
+++ src/services/AlumnosService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3001/api";
+
+/**
+ * Entrada: Id de carrera
+ * Salida: Array con Id de alumnos de esa carrera o array vacio si el id es invalido
+ */
+export const getAlumnosDeCarrera = async (id) => {
+  try {
+    const response = await axios.get(`${baseURL}/alumnos/carrera/${id}`);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+/**
+ * Entrada: Array de Ids de alumno
+ * Salida: Array de datos de alumnos con esos Ids
+ */
+export const getDatosDeAlumnos = async (alumnos) => {
+  try {
+    const response = await axios.post(`${baseURL}/alumnos/statusAcademico/masivo`, alumnos);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
